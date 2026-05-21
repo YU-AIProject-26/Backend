bb153fc2bc61e380f7fceb7b7bc243723dcac90e
Woohyeon Roh <nwh0326@yu.ac.kr>
2026-05-22 06:41:08 +0900

    [수정] 카카오 소셜 로그인 이메일 null 허용을 위해 User 엔터티 제약 완화

--- spring-server/src/main/java/com/acta/springserver/domain/user/entity/User.java
+++ spring-server/src/main/java/com/acta/springserver/domain/user/entity/User.java
@@ -1,13 +1,19 @@
 package com.acta.springserver.domain.user.entity;
 
 import com.acta.springserver.common.entity.BaseEntity;
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
-import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "users")
@@ -19,7 +25,7 @@ public class User extends BaseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false, unique = true, length = 100)
+    @Column(unique = true, length = 100)
     private String email;
 
     @Column(length = 255)
