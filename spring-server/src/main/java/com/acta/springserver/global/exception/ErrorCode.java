4248aca23fcdeac152eafde6d228d1c8470f1db2
Woohyeon Roh <nwh0326@yu.ac.kr>
2026-05-21 18:24:58 +0900

    [수정] 이메일 중복 예외 코드 추가

--- spring-server/src/main/java/com/acta/springserver/global/exception/ErrorCode.java
+++ spring-server/src/main/java/com/acta/springserver/global/exception/ErrorCode.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpStatus;
 public enum ErrorCode {
 
     INVALID_INPUT_VALUE(HttpStatus.BAD_REQUEST, "잘못된 입력값입니다."),
+    EMAIL_ALREADY_EXISTS(HttpStatus.BAD_REQUEST, "이미 사용 중인 이메일입니다."),
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 내부 오류가 발생했습니다.");
 
     private final HttpStatus status;
