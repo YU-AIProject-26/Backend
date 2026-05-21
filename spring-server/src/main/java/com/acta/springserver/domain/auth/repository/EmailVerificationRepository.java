5d5d0106c4d8d15a667520958181f495c07428b1
Woohyeon Roh <nwh0326@yu.ac.kr>
2026-05-21 18:23:00 +0900

    [추가] 이메일 인증 코드 조회 및 삭제용 EmailVerificationRepository 구현

---
+++ spring-server/src/main/java/com/acta/springserver/domain/auth/repository/EmailVerificationRepository.java
@@ -0,0 +1,11 @@
+package com.acta.springserver.domain.auth.repository;
+
+import com.acta.springserver.domain.auth.entity.EmailVerification;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface EmailVerificationRepository extends JpaRepository<EmailVerification, Long> {
+    Optional<EmailVerification> findTopByEmailOrderByCreatedAtDesc(String email);
+    void deleteByEmail(String email);
+}
