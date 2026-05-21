f6e4f9574a0df174d809b8285e388149a88c4923
Woohyeon Roh <nwh0326@yu.ac.kr>
2026-05-22 02:54:00 +0900

    [수정] 내 정보 조회용 사용자 조회 실패 예외 코드 추가

--- spring-server/src/main/java/com/acta/springserver/global/exception/ErrorCode.java
+++ spring-server/src/main/java/com/acta/springserver/global/exception/ErrorCode.java
@@ -17,6 +17,7 @@ public enum ErrorCode {
     EMAIL_ALREADY_VERIFIED(HttpStatus.BAD_REQUEST, "이미 인증이 완료된 이메일입니다."),
     EMAIL_NOT_VERIFIED(HttpStatus.BAD_REQUEST, "이메일 인증이 완료되지 않았습니다."),
     LOGIN_FAILED(HttpStatus.UNAUTHORIZED, "이메일 또는 비밀번호가 올바르지 않습니다."),
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "사용자 정보를 찾을 수 없습니다."),
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 내부 오류가 발생했습니다.");
 
     private final HttpStatus status;
