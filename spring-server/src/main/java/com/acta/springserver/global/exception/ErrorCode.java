a5e37141b1794064ffd3d396e02d5559cf39ae0d
Woohyeon Roh <nwh0326@yu.ac.kr>
2026-05-21 12:56:22 +0900

    [추가] 전역 예외 처리를 위한 ErrorCode enum 구현

---
+++ spring-server/src/main/java/com/acta/springserver/global/exception/ErrorCode.java
@@ -0,0 +1,16 @@
+package com.acta.springserver.global.exception;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum ErrorCode {
+
+    INVALID_INPUT_VALUE(HttpStatus.BAD_REQUEST, "잘못된 입력값입니다."),
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 내부 오류가 발생했습니다.");
+
+    private final HttpStatus status;
+    private final String message;
+}
