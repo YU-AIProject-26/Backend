68f7261580d161ccc5f9612f9be573be40544a32
Woohyeon Roh <nwh0326@yu.ac.kr>
2026-05-21 12:55:17 +0900

    [추가] 사용자 이메일 및 닉네임 중복 조회용 UserRepository 구현

---
+++ spring-server/src/main/java/com/acta/springserver/domain/user/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.acta.springserver.domain.user.repository;
+
+import com.acta.springserver.domain.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    boolean existsByEmail(String email);
+    boolean existsByNickname(String nickname);
+}
