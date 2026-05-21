aab242a11ff4a76298457404d7fd12f97ff51e33
Woohyeon Roh <nwh0326@yu.ac.kr>
2026-05-22 02:38:57 +0900

    [추가] JWT 인증 사용자 정보를 담는 CustomUserPrincipal 구현

---
+++ spring-server/src/main/java/com/acta/springserver/global/security/CustomUserPrincipal.java
@@ -0,0 +1,26 @@
+package com.acta.springserver.global.security;
+
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import java.util.Collection;
+import java.util.List;
+
+@Getter
+public class CustomUserPrincipal {
+
+    private final Long userId;
+    private final String email;
+    private final String role;
+
+    public CustomUserPrincipal(Long userId, String email, String role) {
+        this.userId = userId;
+        this.email = email;
+        this.role = role;
+    }
+
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_" + role));
+    }
+}
